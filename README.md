9a680facf7221708ceb3e8706296c57de7a292c0
Jenifer Mendoza <miau@MacBook-Air-de-Jenifer.local>
2022-05-25 13:04:10 +0200

    primer commit

---
+++ README.md
@@ -0,0 +1,26 @@
+## Practica Java POO
+
+La practica consiste en el desarrollo de un simulador de cajero automático en Java, con POO.
+
+## Cajero automático
+
+Simula las funciones básicas de un cajero automático que son:
+
+- `Opción 1: Consulta de saldo`: 
+- `Opción 2: Retirada de efectivo`: 
+- `Opción 3: Deposito de efectivo`: 
+- `Opción 4: Salir (Finalizar operación)`:
+
+## Práctica POO
+
+El codigo cnotiene:
+
+- El paradigma de la POO.
+- Encapsulamiento
+- Métodos Setter y Guetter.
+- Herencia.
+- Polimorfismo.
+- Ámbito de variables
+
+Nota: No conecta con bases de datos, las variables estan establecidas con un valor de inicio para la práctica.
+
---
+++ src/clases/ClasePadre_Abstracta.java
@@ -0,0 +1,78 @@
+package clases;
+
+import java.util.Scanner;
+
+public abstract class ClasePadre_Abstracta {
+
+    protected int transacciones, retiro, deposito;
+    private static int saldo;
+    Scanner entrada = new Scanner(System.in);
+
+    public void Operaciones() {
+        int bandera = 0;
+        int seleccion = 0;
+        do {
+            do {
+                System.out.println("Seleccione una opción:");
+                System.out.println("     1. Consulta de saldo");
+                System.out.println("     2. Retiro de efectivo.");
+                System.out.println("     3. Deposito de efectivo.");
+                System.out.println("     4. Salir.");
+                seleccion = entrada.nextInt();
+
+                if (seleccion >= 1 && seleccion <= 4) {
+                    bandera = 1;
+                } else {
+                    System.out.println("-------------------------------------------------");
+                    System.out.println("Opción no disponible, vuelva a intentar por favor");
+                    System.out.println("-------------------------------------------------");
+
+                }
+
+            } while (bandera == 0);
+
+            if (seleccion == 1) {
+                ClasePadre_Abstracta mensajero = new ClaseHija_Consulta();
+                mensajero.Transacciones();
+
+            } else if (seleccion == 2) {
+                ClasePadre_Abstracta mensajero = new ClaseHija_Retiro();
+                mensajero.Transacciones();
+
+            } else if (seleccion == 3) {
+                ClasePadre_Abstracta mensajero = new ClaseHija_Deposito();
+                mensajero.Transacciones();
+
+            } else if (seleccion == 4) {
+                System.out.println("----------------------");
+                System.out.println("Gracias, vuelva pronto");
+                System.out.println("----------------------");
+                bandera = 2;
+            }
+
+        } while (bandera != 2);
+
+    }
+
+    //Metodo para solicitar cantidad de retiro
+    public void Retiro(){
+        retiro = entrada.nextInt();
+    }
+
+    //Metodo para solicitr depósito
+    public void Deposito(){
+        deposito = entrada.nextInt();
+    }
+
+    //Metodo abstracto
+    public abstract void Transacciones();
+
+    //Metodos setter y getter
+    public int getSaldo(){
+        return saldo;
+    }
+
+    public void setSaldo(int saldo){
+        ClasePadre_Abstracta.saldo = saldo;
+    }
+}
